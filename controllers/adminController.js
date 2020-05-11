c9417f133254ac8382e52ab63e13761af0fd736c
Jordan Puglisi <jpugli1@students.towson.edu>
2020-05-11 11:59:35 -0400

    user roles now on user table

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,17 +6,30 @@ const api = process.env.SERVER_ADDRESS;
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  axios.all([
-    axios.get(api + '/Account/GetAccounts', { headers: {"Content-Type": "application/json"}}),
-    // axios.get(api + '/Inventory/GetStores', { headers: {"Content-Type": "application/json"}})
-  ])
-  .then(axios.spread((usersRes) => {
-    console.log(usersRes.data)
-    res.render("admin/index", {users: usersRes.data});
-  }))
-  .catch(err => {
-    console.log(err);
-  });
+  var token;
+  if (req.cookies.user) {
+    token = req.cookies.user;
+    axios.all([
+      axios.get(api + '/Account/GetAccounts', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
+      // axios.get(api + '/Inventory/GetStores', { headers: {  "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }})
+    ])
+    .then(axios.spread((usersRes) => {
+      usersRes.data.forEach(user => {
+        if (user.IsActive && user.Roles[0]) {
+          console.log(user.Roles[0].RoleName);
+        }
+      });
+      res.render("admin/index", {users: usersRes.data});
+    }))
+    .catch(err => {
+      console.log(err);
+      if (err.response.status == 401) {
+        res.redirect("user/login");
+      }
+    });
+  } else {
+    res.render("user/login");
+  }
 });
 
 module.exports = router;
