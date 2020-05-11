94a1e67cde0806e7ddf433434eb042f364369c9a
Jordan Puglisi <jpugli1@students.towson.edu>
2020-05-11 11:35:25 -0400

    fix functionality for accessories

--- controllers/accessoriesController.js
+++ controllers/accessoriesController.js
@@ -5,43 +5,63 @@ const api = process.env.SERVER_ADDRESS;
 
 /* GET index page. */
 router.get('/', function (req, res, next) {
-  axios({
-    url: api + '/Inventory/GetAccessories',
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json"
-    }, 
-  })
-  .then(response => {
-    res.render('accessories/index', { data: response.data });
-  })
-  .catch(error => {
-    console.log(error);
-  });
+  var token;
+  if (req.cookies.user) {
+    token = req.cookies.user;
+    axios({
+      url: api + '/Inventory/GetAccessories',
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }, 
+    })
+    .then(response => {
+      res.render('accessories/index', { data: response.data });
+    })
+    .catch(error => {
+      console.log(error);
+      if (error.response.status == 401) {
+        res.redirect("user/login");
+      }
+    });
+  } else {
+    res.redirect("/user/login");
+  }
 });
 
 // Add a new accessory
 router.post('/add', function (req, res, next) {
-  axios({
-    url: api + '/Inventory/AddAccessory',
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    }, 
-    params: {
-      name: req.body.name,
-      description: req.body.description,
-      serialNumber: req.body.serialNumber,
-      retailPrice: req.body.retailPrice,
-      itemManufacturerId: req.body.itemManufacturerId
-    }
-  })
-  .then(response => {
-    res.redirect('/accessories');
-  })
-  .catch(error => {
-    console.log(error);
-  });
+  var token;
+  if (req.cookies.user) {
+    token = req.cookies.user;
+    axios({
+      url: api + '/Admin/AddAccessory',
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }, 
+      params: {
+        name: req.body.name,
+        description: req.body.description,
+        serialNumber: req.body.serialNumber,
+        retailPrice: req.body.retailPrice,
+        itemManufacturerId: req.body.itemManufacturerId
+      }
+    })
+    .then(response => {
+      res.redirect('/accessories');
+    })
+    .catch(error => {
+      console.log(error);
+      if (error.response.status == 401) {
+        res.redirect("user/login");
+      }
+    });
+  } else {
+    res.render("user/login");
+  }
 });
 
 // Edit an accessory
@@ -49,23 +69,33 @@ router.post('/add', function (req, res, next) {
 
 // Delete an accessory
 router.post('/delete', function (req, res, next) {
-  axios({
-    url: api + '/Inventory/DeleteAccessory',
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    }, 
-    params: {
-      id: req.body.id
-    }
-  })
-  .then(response => {
-    console.log(response.data);
-    res.redirect('/accessories');
-  })
-  .catch(error => {
-    console.log(error);
-  });
+  var token;
+  if (req.cookies.user) {
+    token = req.cookies.user;
+    axios({
+      url: api + '/Admin/DeleteAccessory',
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }, 
+      params: {
+        id: req.body.id
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+      res.redirect('/accessories');
+    })
+    .catch(error => {
+      console.log(error);
+      if (error.response.status == 401) {
+        res.redirect("user/login");
+      }
+    });
+  } else {
+    res.render("user/login");
+  }
 });
 
 module.exports = router;
