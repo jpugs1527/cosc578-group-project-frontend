0d412829432f7b82edc2422cced0d9f20399a8a2
Jordan Puglisi <jpugli1@students.towson.edu>
2020-05-12 16:54:17 -0400

    final touches

--- controllers/accessoriesController.js
+++ controllers/accessoriesController.js
@@ -45,18 +45,48 @@ router.post("/add", function (req, res, next) {
         serialNumber: req.body.serialNumber,
         retailPrice: req.body.retailPrice,
         itemManufacturerId: req.body.itemManufacturerId,
-        storeId: req.body.store ? req.body.store : ""
+        storeId: req.body.store ? req.body.store : null
       },
     })
-      .then((response) => {
+    .then((response) => {
+      if (req.body.store != null) {
+        axios.all([
+          axios.get(api + '/Inventory/GetStores', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
+          axios.get(api + '/Inventory/GetItemManufacturer', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
+          axios.get(api + '/Inventory/GetStoreInventory', 
+            { headers: { 
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json" 
+              },
+              params: {
+                storeId: req.body.store 
+              }
+            })
+        ])
+        .then(axios.spread((storesRes, manufacturersRes, inventoryRes) => {
+          if (inventoryRes.data.Items) {
+            storeInventory = inventoryRes.data.Items.Accessories
+          } else {
+            storeInventory = [];
+          }
+          res.render("accessories/index", {stores: storesRes.data, manufacturers: manufacturersRes.data, data: storeInventory, currStore: req.body.store});
+        }))
+        .catch(err => {
+          console.log(err);
+          if (err.response.status == 401) {
+            res.redirect("Unauthorized");
+          }
+        });
+      } else {
         res.redirect("/accessories");
-      })
-      .catch((error) => {
-        console.log(error);
-        if (error.response.status == 401) {
-          res.send("Unauthorized");
-        }
-      });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      if (error.response.status == 401) {
+        res.send("Unauthorized");
+      }
+    });
   } else {
     res.render("user/login");
   }
