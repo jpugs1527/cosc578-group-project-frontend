a210b3ee0f1bcafbd2d93920c37d82a90bf5e352
Jordan Puglisi <jpugli1@students.towson.edu>
2020-05-11 22:53:40 -0400

    add store functionality

--- controllers/storesController.js
+++ controllers/storesController.js
@@ -5,8 +5,35 @@ require("dotenv").config();
 const api = process.env.SERVER_ADDRESS;
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  
+router.post("/add", function (req, res, next) {
+  var storeObj = req.body;
+  var token;
+  if (req.cookies.user) {
+    token = req.cookies.user;
+    axios({
+      url: api + '/Admin/AddStore',
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }, 
+      params: {
+        storeName: storeObj.storeName,
+        storePercentage: storeObj.storePercentage
+      }
+    })
+    .then(response => {
+      res.redirect('/admin');
+    })
+    .catch(error => {
+      console.log(error);
+      if (error.response.status == 401) {
+        res.redirect("user/login");
+      }
+    });
+  } else {
+    res.redirect("/user/login");
+  }
 });
 
 module.exports = router;
