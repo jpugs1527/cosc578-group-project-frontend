ba9f3a165f673f286d381b86fbe2d16ee1fa3486
Jordan Puglisi <jpugli1@students.towson.edu>
2020-05-12 14:59:37 -0400

    add filtering by store capabilities

--- controllers/accessoriesController.js
+++ controllers/accessoriesController.js
@@ -10,10 +10,11 @@ router.get("/", function (req, res, next) {
     token = req.cookies.user;
     axios.all([
       axios.get(api + '/Inventory/GetAccessories', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
-      axios.get(api + '/Inventory/GetItemManufacturer', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }})
+      axios.get(api + '/Inventory/GetItemManufacturer', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
+      axios.get(api + '/Inventory/GetStores', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }})
     ])
-    .then(axios.spread((accessoriesRes, manufacturersRes) => {
-      res.render("accessories/index", {data: accessoriesRes.data, manufacturers: manufacturersRes.data});
+    .then(axios.spread((accessoriesRes, manufacturersRes, storesRes) => {
+      res.render("accessories/index", {data: accessoriesRes.data, manufacturers: manufacturersRes.data, stores: storesRes.data});
     }))
     .catch(err => {
       console.log(err);
@@ -44,6 +45,7 @@ router.post("/add", function (req, res, next) {
         serialNumber: req.body.serialNumber,
         retailPrice: req.body.retailPrice,
         itemManufacturerId: req.body.itemManufacturerId,
+        storeId: req.body.store ? req.body.store : ""
       },
     })
       .then((response) => {
@@ -87,6 +89,45 @@ router.post("/delete", function (req, res, next) {
           res.send("Unauthorized");
         }
       });
+  } else {
+    res.render("user/login");
+  }
+});
+
+// Filter results by store
+router.post("/filter", function (req, res, next) {
+  var token;
+  var storeInventory;
+  if (req.cookies.user) {
+    token = req.cookies.user;
+    axios.all([
+      axios.get(api + '/Inventory/GetStores', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
+      axios.get(api + '/Inventory/GetItemManufacturer', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
+      axios.get(api + '/Inventory/GetStoreInventory', 
+        { headers: { 
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json" 
+          },
+          params: {
+            storeId: req.body.storeId 
+          }
+        })
+    ])
+    .then(axios.spread((storesRes, manufacturersRes, inventoryRes) => {
+      if (inventoryRes.data.Items) {
+        storeInventory = inventoryRes.data.Items.Accessories
+      } else {
+        storeInventory = [];
+      }
+      console.log(storesRes.data);
+      res.render("accessories/index", {stores: storesRes.data, manufacturers: manufacturersRes.data, data: storeInventory, currStore: req.body.storeId});
+    }))
+    .catch(err => {
+      console.log(err);
+      if (err.response.status == 401) {
+        res.redirect("Unauthorized");
+      }
+    });
   } else {
     res.render("user/login");
   }
--- controllers/phonesController.js
+++ controllers/phonesController.js
@@ -5,47 +5,49 @@ const api = process.env.SERVER_ADDRESS;
 const jwtDecode = require("jwt-decode");
 
 /* GET index page. */
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   var token;
   if (req.cookies.user) {
     token = req.cookies.user;
     axios.all([
       axios.get(api + '/Inventory/GetDevices', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
-      axios.get(api + '/Inventory/GetItemManufacturer', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }})
+      axios.get(api + '/Inventory/GetItemManufacturer', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
+      axios.get(api + '/Inventory/GetStores', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }})
     ])
-    .then(axios.spread((devicesRes, manufacturersRes) => {
-      res.render("phones/index", {data: devicesRes.data, manufacturers: manufacturersRes.data});
+    .then(axios.spread((devicesRes, manufacturersRes, storesRes) => {
+      res.render("phones/index", {data: devicesRes.data, manufacturers: manufacturersRes.data, stores: storesRes.data});
     }))
     .catch(err => {
       console.log(err);
       if (err.response.status == 401) {
-        res.send("Unauthorized");
+        res.redirect("Unauthorized");
       }
     });
   } else {
-    res.render("/user/login");
+    res.render("user/login");
   }
 });
 
 // Add a new device
-router.post('/add', function (req, res, next) {
+router.post("/add", function (req, res, next) {
   var token;
   if (req.cookies.user) {
     token = req.cookies.user;
     axios({
-      url: api + '/Admin/AddDevice',
+      url: api + "/Admin/AddDevice",
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${token}`,
-        "Content-Type": "application/json"
-      }, 
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
       params: {
         name: req.body.name,
         serialNumber: req.body.serialNumber,
         imei: req.body.imei,
         retailPrice: req.body.retailPrice,
-        itemManufacturerId: req.body.itemManufacturerId
-      }
+        itemManufacturerId: req.body.itemManufacturerId,
+        storeId: req.body.store ? req.body.store : ""
+      },
     })
     .then(response => {
       res.redirect('/phones');
@@ -62,33 +64,71 @@ router.post('/add', function (req, res, next) {
 });
 
 // Delete an accessory
-router.post('/delete', function (req, res, next) {
+router.post("/delete", function (req, res, next) {
   var token;
   if (req.cookies.user) {
     token = req.cookies.user;
     axios({
-      url: api + '/Admin/DeleteDevice',
+      url: api + "/Admin/DeleteDevice",
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${token}`,
-        "Content-Type": "application/json"
-      }, 
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
       params: {
-        id: req.body.id
+        id: req.body.id,
+      },
+    })
+      .then((response) => {
+        res.redirect("/phones");
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.response.status == 401) {
+          res.send("Unauthorized");
+        }
+      });
+  } else {
+    res.redirect("/user/login");
+  }
+});
+
+// Filter results by store
+router.post("/filter", function (req, res, next) {
+  var token;
+  var storeInventory;
+  if (req.cookies.user) {
+    token = req.cookies.user;
+    axios.all([
+      axios.get(api + '/Inventory/GetStores', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
+      axios.get(api + '/Inventory/GetItemManufacturer', { headers: { "Authorization": `Bearer ${token}`, "Content-Type": "application/json" }}),
+      axios.get(api + '/Inventory/GetStoreInventory', 
+        { headers: { 
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json" 
+          },
+          params: {
+            storeId: req.body.storeId 
+          }
+        })
+    ])
+    .then(axios.spread((storesRes, manufacturersRes, inventoryRes) => {
+      if (inventoryRes.data.Items) {
+        storeInventory = inventoryRes.data.Items.Devices
+      } else {
+        storeInventory = [];
       }
-    })
-    .then(response => {
-      res.redirect('/phones');
-    })
-    .catch(error => {
-      console.log(error);
-      if (error.response.status == 401) {
-        res.send("Unauthorized");
+      res.render("phones/index", {stores: storesRes.data, manufacturers: manufacturersRes.data, data: storeInventory, currStore: req.body.storeId});
+    }))
+    .catch(err => {
+      console.log(err);
+      if (err.response.status == 401) {
+        res.redirect("Unauthorized");
       }
     });
   } else {
-    res.redirect("/user/login");
-  }  
+    res.render("user/login");
+  }
 });
 
 module.exports = router;
